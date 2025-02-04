afce8670441a9247a14f3bf17cb040ef0ce007ff
Gintare <lapiene.g@gmail.com>
2025-02-04 15:04:51 +0200

    2 commit

---
+++ js/variables/06-numbers.js
@@ -0,0 +1,99 @@
+/* number
+// sveikieji, desimtainiai
+// teigiami, neigiami
+// Infinity ( butina didzioji raide - begalybe), NaN (none)
+
+*/
+const n1 = 1;
+console.log(n1);
+
+const n2 = -5;
+console.log(n2);
+
+const n3 = 3.14;
+console.log(n3);
+
+const n4 = -2.727;
+console.log(n4);
+
+const n5 = Infinity;
+console.log(n5);
+
+const n6 = -Infinity;
+console.log(n6);
+
+const n7 = -9999999
+console.log(n7);
+
+const n8 = NaN
+console.log(n8);
+
+console.log();
+
+console.log(' Labas rytas' / 2);
+console.log(' Labas rytas' * 2);
+console.log(' Labas rytas' - 2);
+console.log(' Labas rytas' % 2);
+console.log(' Labas rytas' + 2);
+
+console.log();
+console.log(7 / 5);
+console.log(7 - 5);
+console.log(7 % 5);
+console.log(7 + 5);
+console.log(7 * 5);
+console.log(7 ** 5);
+
+
+console.log();
+console.log(2 ** 0);
+console.log(2 ** 1);
+console.log(2 ** 2);
+console.log(2 ** 3);
+console.log(2 ** 4);
+console.log(2 ** 5);
+
+// Math - matematiniu funkciju "biblioteka"
+
+// Matematines konstantos
+console.log();
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+// Math. abs ()
+console.log();
+console.log(Math.abs(5));
+console.log(Math.abs(-5));
+
+// Trigonometrija - naudoja radianus
+console.log();
+console.log(Math.sin(0));
+console.log(Math.cos(0));
+console.log(Math.tan(0));
+
+// kubine saknis
+console.log();
+console.log(Math.cbrt(8));
+console.log(Math.cbrt(64));
+
+// Apvalinimas ceil
+console.log();
+console.log(Math.ceil(10));
+console.log(Math.ceil(9.5));
+console.log(Math.ceil(9.9));
+console.log(Math.ceil(9.1));
+console.log(Math.ceil(9.000001));
+
+// Apvalinimas floor
+console.log();
+console.log(Math.floor(10));
+console.log(Math.floor(9.5));
+console.log(Math.floor(9.9));
+console.log(Math.floor(9.1));
+console.log(Math.floor(9.000001));
+
+// Apvalinimas round
+console.log();
+console.log(Math.round(10));
+console.log(Math.round(9.5));
